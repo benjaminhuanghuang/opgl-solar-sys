7e151053e9cc16dd5a02dac7df9db971c488ddf7
benjaminhuanghuang <benjaminhuanghuang@gmail.com>
2021-03-25 21:08:19 -0700

    add imgui

--- cpp-version/Gui.h
+++ cpp-version/Gui.h
@@ -2,9 +2,9 @@
 #define GLSOLARSYSTEM_GUI_H
 
 #include "../display/Display.h"
-#include "../../lib/imgui/imgui.h"
-#include "../../lib/imgui/imgui_impl_glfw.h"
-#include "../../lib/imgui/imgui_impl_opengl3.h"
+#include "lib/imgui/imgui.h"
+#include "lib/imgui/imgui_impl_glfw.h"
+#include "lib/imgui/imgui_impl_opengl3.h"
 
 #include <string>
 
